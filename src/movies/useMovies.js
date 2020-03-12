833a11f521f415a6f3fb7a24e6fecab3a397948d
Caio Lucio Sousa Duarte <284097@sga.pucminas.br>
2020-03-11 21:31:25 -0300

    importacao do Service

--- src/movies/useMovies.js
+++ src/movies/useMovies.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { MoviesService } from '../services/MoviesService';
 
 export const useMovies = () => {
     const [movies, setMovies] = useState({data: {results: [] }});
